74431c851361982c00477b9e4c618f5e760e6ab0
RoboMolnar <128727112+RoboMolnar@users.noreply.github.com>
2023-03-23 17:58:50 +0100

    Add files via upload

---
+++ command.php
@@ -0,0 +1,25 @@
+<?php
+$menu = [
+    'home' => [
+        'name' => 'Domov',
+        'path' => 'index.php',
+    ],
+    'portfolio' => [
+        'name' => 'Portfólio',
+        'path' => 'portfolio.php',
+    ],
+    'qna' => [
+        'name' => 'Q&A',
+        'path' => 'qna.php',
+    ],
+    'kontakt' => [
+        'name' => 'Kontakt',
+        'path' => 'kontakt.php',
+    ]
+];
+$file = fopen('header.csv', 'w');
+foreach ($menu as $hodnota) {
+    fputcsv($file, $hodnota);
+}
+fclose($file);
+?>
